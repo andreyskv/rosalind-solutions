3308c92684a33715cd27a9d6f9bbb03fa737d254
andrey.skv <andrey.skv@gmail.com>
2019-12-09 15:12:42 -0800

    Solved Finding a Shared Motif

---
+++ LCSM.py
@@ -0,0 +1,30 @@
+# Finding a Shared Motif
+
+nucleotides = ['A', 'C', 'G', 'T']
+
+# Read FASTA format file
+def read_fasta(filename):
+    file = open(filename)
+    sample = file.read()
+    # Convert FASTA string into dictonary
+    entries = dict(map(lambda e: [i.replace('\n', '') for i in e.split('\n', 1)][::-1], sample.lstrip('>').split('>')))
+    return list(entries.keys())
+
+
+dna_list = read_fasta('LCSM.txt')
+dna_list_length = len(dna_list)
+# print(dna_list)
+
+candidates = nucleotides.copy()  # Start with 1 letter nucleotides as common string candidates
+new_candidates = ['']  # Initialize with not empty list
+while new_candidates:  # Search until the new candidates list has anything in it
+    new_candidates.clear()  # Reset new candidates list
+    for c in candidates:
+        for n in nucleotides:
+            new_c = c + n  # Append a nucleotide letter to form a new candidate
+            if len([n for n in dna_list if new_c in n]) == len(dna_list):  # Check if new candidate passes test and contain in all strings
+                new_candidates.append(new_c)  # Store it
+    if new_candidates:  # Swap new candidates with candidates list. Candidates are now former new candidates.
+        candidates, new_candidates = new_candidates, candidates
+
+print(candidates)
