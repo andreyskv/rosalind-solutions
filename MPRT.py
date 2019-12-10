4f2cad5a575420763d0f708ecc81b740ab6f1033
andrey.skv <andrey.skv@gmail.com>
2019-12-10 09:07:26 -0800

    Solved Finding a Protein Motif (fetch from uniprot database)

---
+++ MPRT.py
@@ -0,0 +1,34 @@
+# Finding a Protein Motif
+
+from urllib.request import urlopen
+file = open('MPRT.txt')
+sample = file.read()
+protein_ids = sample.split('\n')
+
+def get_protein(id):
+    # print(pid)
+    url = 'https://www.uniprot.org/uniprot/{}.fasta'.format(id)
+    data = urlopen(url).read()
+    body = data.decode('ascii')
+    p = [i.replace('\n', '') for i in body.lstrip('>').split('\n', 1)][1]  # Read one entry FASTA format
+    return p
+
+
+result = {}
+for pid in protein_ids:
+    result[pid] = []
+    p = get_protein(pid)
+    pos = 0
+    while True:
+        pos = p.find('N', pos)
+        if pos == -1 or pos >= (len(p) - 3):
+            break;
+
+        if p[pos+1] != 'P' and p[pos+3] != 'P' and (p[pos+2] == 'S' or p[pos+2] == 'T'):  # Verify match to 'N{P}[ST]{P}'
+            result[pid].append(pos+1)
+        pos += 1
+
+
+for r in filter(lambda r: result[r], result):
+    print(r)
+    print(' '.join(str(i) for i in result[r]))
