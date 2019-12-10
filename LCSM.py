3a996b0a62f2ae9fc32fab4326eda0e80977e759
andrey.skv <andrey.skv@gmail.com>
2019-12-10 09:07:58 -0800

    Refactor lcsm

--- LCSM.py
+++ LCSM.py
@@ -15,16 +15,16 @@ dna_list = read_fasta('LCSM.txt')
 dna_list_length = len(dna_list)
 # print(dna_list)
 
-candidates = nucleotides.copy()  # Start with 1 letter nucleotides as common string candidates
-new_candidates = ['']  # Initialize with not empty list
-while new_candidates:  # Search until the new candidates list has anything in it
-    new_candidates.clear()  # Reset new candidates list
-    for c in candidates:
+picks = nucleotides.copy()  # Start with 1 letter nucleotides as common string candidates
+new_picks = ['']  # Initialize with not empty list
+while new_picks:  # Search until the new candidates list has anything in it
+    new_picks.clear()  # Reset new candidates list
+    for p in picks:
         for n in nucleotides:
-            new_c = c + n  # Append a nucleotide letter to form a new candidate
-            if len([n for n in dna_list if new_c in n]) == len(dna_list):  # Check if new candidate passes test and contain in all strings
-                new_candidates.append(new_c)  # Store it
-    if new_candidates:  # Swap new candidates with candidates list. Candidates are now former new candidates.
-        candidates, new_candidates = new_candidates, candidates
+            new_p = p + n  # Append a nucleotide letter to form a new candidate
+            if len([n for n in dna_list if new_p in n]) == len(dna_list):  # Check if new candidate passes test and contain in all strings
+                new_picks.append(new_p)  # Store it
+    if new_picks:  # Swap new candidates with candidates list. Candidates are now former new candidates.
+        picks, new_picks = new_picks, picks
 
-print(candidates)
+print(picks)
