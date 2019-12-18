0e1075c61f628951a97192ad98600c432a42097a
andrey.skv <andrey.skv@gmail.com>
2019-12-17 20:12:36 -0800

    REAR optimize breakpoints finding

--- REAR.py
+++ REAR.py
@@ -17,21 +17,18 @@ def normalize(x, y):
 
 # Find breakpoints for the list. Breakpoints are places where number jumps more than 1
 # Something like described here http://scholarworks.sjsu.edu/cgi/viewcontent.cgi?article=1103&context=etd_projects
-def breakpoint_cnt(x):
-    count = 0
+def breakpt(x):
+    # breakpoints = [i-1 for i in range(1, N + 1) if abs(x[i] - x[i - 1]) > 1]  this is slower (??)
     breakpoints = []
-    for i in range(0, N + 1):
-
-        left = x[i - 1] if i > 0 else 0
-        right = x[i] if i < N else N+1
-
-        if abs(right - left) > 1:
-            # count += 1
+    x.append(N+1)
+    for i in range(1, N + 1):
+        if abs(x[i] - x[i - 1]) > 1:
             breakpoints.append(i-1)
+    x.pop()
     return breakpoints
 
 
-# Hamming distance betwen x and y is count of places where values don't match
+# # Hamming distance betwen x and y is number of places where values don't match
 def hamming_distance(x, y):
     return sum([j != x[i] for i, j in enumerate(y)])
 
@@ -40,14 +37,14 @@ def search(dist, c):
     global sol
 
     # new_dist = hamming_distance(a1, b1)  # Use hamming distance as pruning method (weaker but much faster convergence method)
-    bps = breakpoint_cnt(b)               # Use minimal breakpoint distance method (much stronger method but slow convergence for depth 9)
+    bps = breakpt(b)                # Use minimal breakpoint distance method (much stronger method but slow convergence for depth 9)
     new_dist = len(bps)
-    if new_dist >= dist:  # Pruning to avoid full recursion. We cut this branch and exit if the new distance is not better than on the previous step.
+    if new_dist >= dist:  # Pruning to avoid full recursion. If the new distance is not better than on the previous step then exit branch.
         return
-    elif new_dist == 0:   # We are done! The distance is 0 which means the new string matches the target exactly.
-        sol = min(sol, c)
+    elif new_dist == 0:    # We are done! The distance is 0 which means the new string matches the target exactly.
+        sol = min(sol, c)  # min is not necessary, but for safety
         return
-    elif c == N-2 or c >= sol: # If 8 and we checked new_dist is not 0 than solution can only be 9. We cut it here.
+    elif c == N-2 or c >= sol:  # If 8 and we checked new_dist is not 0 then solution can only be 9 so exit right away.
         return
     else:
         c += 1             # The new distance is greater than the previous so we increment the counter and continue with this branch.
@@ -58,12 +55,12 @@ def search(dist, c):
             if j - i > 1:
                 b[i:j+1] = b[i:j+1][::-1]
                 search(new_dist, c)
-                b[i:j + 1] = b[i:j + 1][::-1]  # Intead of creating a copy of the list we revert b to original state for the next step
+                b[i:j + 1] = b[i:j + 1][::-1]  # Intead of creating a copy of the list we revert b to the original state for the next step
 
 
 result = []
 for p in pairs:
-    K = 0
+
     sol = 9
     a = [int(x) for x in p[0].split(' ')]
     b = [int(x) for x in p[1].split(' ')]
@@ -74,15 +71,14 @@ for p in pairs:
     b = normalize(b,a)
     a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    # start = hamming_distance(a, b)
-    start_bps = breakpoint_cnt(b)
+    start_bps = breakpt(b)
     start = len(start_bps)
     search(start + 1, 0)
 
     print(sol)
     result.append(sol)
 
-print (' '.join(str(x) for x in result))
+print(' '.join(str(x) for x in result))
 
 print("time elapsed: {:.2f}s".format(time.time() - start_time))
 
