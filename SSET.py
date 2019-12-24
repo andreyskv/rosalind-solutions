4f80b36d653d140733196eeb48ef59b0be750a30
andrey.skv <andrey.skv@gmail.com>
2019-12-24 13:26:47 -0800

    comment to SSET

--- SSET.py
+++ SSET.py
@@ -4,3 +4,5 @@ N = 821
 
 # Solution is the sum of combinations for each subset of length k in N.
 print(sum(comb(N, i) for i in range(0, N + 1))% 1000000)
+
+# The best solution found later on the solutions board is simply pow(2, N, 1000000)
